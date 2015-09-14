f95ceec9aef8b333cc019f25b22a6c8fa8cd59b2
Olivier Favre <olivier@wonderpush.com>
2015-09-15 01:12:59 +0200

    Documentation site for v1.1.1.2

---
+++ 1.1.1.2/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "com.wonderpush.sdk", "com/wonderpush/sdk/package-summary.html", [ [ "Interfaces", null, [ [ "WonderPushInitializer", "com/wonderpush/sdk/WonderPushInitializer.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "WonderPush", "com/wonderpush/sdk/WonderPush.html", null, "" ], [ "WonderPushBroadcastReceiver", "com/wonderpush/sdk/WonderPushBroadcastReceiver.html", null, "" ], [ "WonderPushOnUpgradeReceiver", "com/wonderpush/sdk/WonderPushOnUpgradeReceiver.html", null, "" ], [ "WonderPushService", "com/wonderpush/sdk/WonderPushService.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ 1.1.1.2/reference/lists.js
@@ -0,0 +1,9 @@
+var DATA = [
+      { id:0, label:"com.wonderpush.sdk", link:"com/wonderpush/sdk/package-summary.html", type:"package" },
+      { id:1, label:"com.wonderpush.sdk.WonderPush", link:"com/wonderpush/sdk/WonderPush.html", type:"class" },
+      { id:2, label:"com.wonderpush.sdk.WonderPushBroadcastReceiver", link:"com/wonderpush/sdk/WonderPushBroadcastReceiver.html", type:"class" },
+      { id:3, label:"com.wonderpush.sdk.WonderPushInitializer", link:"com/wonderpush/sdk/WonderPushInitializer.html", type:"class" },
+      { id:4, label:"com.wonderpush.sdk.WonderPushOnUpgradeReceiver", link:"com/wonderpush/sdk/WonderPushOnUpgradeReceiver.html", type:"class" },
+      { id:5, label:"com.wonderpush.sdk.WonderPushService", link:"com/wonderpush/sdk/WonderPushService.html", type:"class" }
+
+    ];
