e85c92ad1b95ddf67a01316e7afb66d204953363
Olivier Favre <olivier@wonderpush.com>
2016-06-20 10:19:59 +0200

    Prevent NoClassDefFoundError on pre Android 14 devices

--- sdk/src/main/java/com/wonderpush/sdk/ActivityLifecycleMonitor.java
+++ sdk/src/main/java/com/wonderpush/sdk/ActivityLifecycleMonitor.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.app.Application;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.WeakHashMap;
 
@@ -12,28 +13,23 @@ import java.util.WeakHashMap;
  * Monitors activities lifecycle operations, which are evidences of user interactions.
  * @link http://www.mjbshaw.com/2012/12/determining-if-your-android-application.html
  */
-@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-class ActivityLifecycleMonitor implements Application.ActivityLifecycleCallbacks {
+class ActivityLifecycleMonitor {
 
-    private static final ActivityLifecycleMonitor sSingleton = new ActivityLifecycleMonitor();
+    private static final Monitor sSingleton;
     private static boolean sActivityLifecycleCallbacksRegistered;
     private static WeakHashMap<Activity, Object> sTrackedActivities = new WeakHashMap<>();
 
-    private int createCount;
-    private int startCount;
-    private int resumeCount;
-    private int pausedCount;
-    private int stopCount;
-    private int destroyCount;
-
-    private long createFirstDate;
-    private long startFirstDate;
-    private long resumeFirstDate;
-    private long pausedLastDate;
-    private long stopLastDate;
-    private long destroyLastDate;
-
-    private Activity lastResumedActivity;
+    static {
+        Monitor monitor = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            try {
+                monitor = new Monitor();
+            } catch (Throwable ex) { // java.lang.NoClassDefFoundError: com.wonderpush.sdk.ActivityLifecycleMonitor$Monitor
+                Log.d(WonderPush.TAG, "Cannot instantiate the ActivityLifecycleMonitor, pre Android 14 version?", ex);
+            }
+        }
+        sSingleton = monitor;
+    }
 
     private static boolean isSupported() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
@@ -53,6 +49,7 @@ class ActivityLifecycleMonitor implements Application.ActivityLifecycleCallbacks
     protected static Activity getCurrentActivity() {
         Activity candidate = null;
         if (sActivityLifecycleCallbacksRegistered
+                && sSingleton != null
                 && sSingleton.hasResumedActivities()) {
             candidate = sSingleton.getLastResumedActivity();
         }
@@ -67,107 +64,128 @@ class ActivityLifecycleMonitor implements Application.ActivityLifecycleCallbacks
         return candidate;
     }
 
-    @Override
-    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        if (!hasCreatedActivities()) {
-            createFirstDate = TimeSync.getTime();
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    static class Monitor implements Application.ActivityLifecycleCallbacks {
+
+        private int createCount;
+        private int startCount;
+        private int resumeCount;
+        private int pausedCount;
+        private int stopCount;
+        private int destroyCount;
+
+        private long createFirstDate;
+        private long startFirstDate;
+        private long resumeFirstDate;
+        private long pausedLastDate;
+        private long stopLastDate;
+        private long destroyLastDate;
+
+        private Activity lastResumedActivity;
+
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+            if (!hasCreatedActivities()) {
+                createFirstDate = TimeSync.getTime();
+            }
+            ++createCount;
+            WonderPush.showPotentialNotification(activity, activity.getIntent());
         }
-        ++createCount;
-        WonderPush.showPotentialNotification(activity, activity.getIntent());
-    }
 
-    @Override
-    public void onActivityStarted(Activity activity) {
-        if (createCount == 0) {
-            // The monitor was probably setup inside a Activity.onCreate() call
-            this.onActivityCreated(activity, null);
+        @Override
+        public void onActivityStarted(Activity activity) {
+            if (createCount == 0) {
+                // The monitor was probably setup inside a Activity.onCreate() call
+                this.onActivityCreated(activity, null);
+            }
+            if (!hasStartedActivities()) {
+                startFirstDate = TimeSync.getTime();
+            }
+            ++startCount;
+            WonderPush.onInteraction();
         }
-        if (!hasStartedActivities()) {
-            startFirstDate = TimeSync.getTime();
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+            if (!hasResumedActivities()) {
+                resumeFirstDate = TimeSync.getTime();
+            }
+            lastResumedActivity = activity;
+            ++resumeCount;
+            WonderPush.onInteraction();
         }
-        ++startCount;
-        WonderPush.onInteraction();
-    }
 
-    @Override
-    public void onActivityResumed(Activity activity) {
-        if (!hasResumedActivities()) {
-            resumeFirstDate = TimeSync.getTime();
+        @Override
+        public void onActivityPaused(Activity activity) {
+            ++pausedCount;
+            if (!hasResumedActivities()) {
+                pausedLastDate = TimeSync.getTime();
+            }
+            WonderPush.onInteraction();
         }
-        lastResumedActivity = activity;
-        ++resumeCount;
-        WonderPush.onInteraction();
-    }
 
-    @Override
-    public void onActivityPaused(Activity activity) {
-        ++pausedCount;
-        if (!hasResumedActivities()) {
-            pausedLastDate = TimeSync.getTime();
+        @Override
+        public void onActivityStopped(Activity activity) {
+            ++stopCount;
+            if (!hasStartedActivities()) {
+                stopLastDate = TimeSync.getTime();
+            }
+            WonderPush.onInteraction();
         }
-        WonderPush.onInteraction();
-    }
 
-    @Override
-    public void onActivityStopped(Activity activity) {
-        ++stopCount;
-        if (!hasStartedActivities()) {
-            stopLastDate = TimeSync.getTime();
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
         }
-        WonderPush.onInteraction();
-    }
 
-    @Override
-    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-    }
-
-    @Override
-    public void onActivityDestroyed(Activity activity) {
-        ++destroyCount;
-        if (!hasCreatedActivities()) {
-            destroyLastDate = TimeSync.getTime();
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+            ++destroyCount;
+            if (!hasCreatedActivities()) {
+                destroyLastDate = TimeSync.getTime();
+            }
+            WonderPush.onInteraction();
         }
-        WonderPush.onInteraction();
-    }
 
-    protected boolean hasResumedActivities() {
-        return resumeCount > pausedCount;
-    }
+        protected boolean hasResumedActivities() {
+            return resumeCount > pausedCount;
+        }
 
-    protected boolean hasStartedActivities() {
-        return startCount > stopCount;
-    }
+        protected boolean hasStartedActivities() {
+            return startCount > stopCount;
+        }
 
-    protected boolean hasCreatedActivities() {
-        return createCount > destroyCount;
-    }
+        protected boolean hasCreatedActivities() {
+            return createCount > destroyCount;
+        }
 
-    protected Activity getLastResumedActivity() {
-        return lastResumedActivity;
-    }
+        protected Activity getLastResumedActivity() {
+            return lastResumedActivity;
+        }
 
-    protected long getCreateFirstDate() {
-        return createFirstDate;
-    }
+        protected long getCreateFirstDate() {
+            return createFirstDate;
+        }
 
-    protected long getStartFirstDate() {
-        return startFirstDate;
-    }
+        protected long getStartFirstDate() {
+            return startFirstDate;
+        }
 
-    protected long getResumeFirstDate() {
-        return resumeFirstDate;
-    }
+        protected long getResumeFirstDate() {
+            return resumeFirstDate;
+        }
 
-    protected long getPausedLastDate() {
-        return pausedLastDate;
-    }
+        protected long getPausedLastDate() {
+            return pausedLastDate;
+        }
 
-    protected long getStopLastDate() {
-        return stopLastDate;
-    }
+        protected long getStopLastDate() {
+            return stopLastDate;
+        }
 
-    protected long getDestroyLastDate() {
-        return destroyLastDate;
+        protected long getDestroyLastDate() {
+            return destroyLastDate;
+        }
+
     }
 
 }
