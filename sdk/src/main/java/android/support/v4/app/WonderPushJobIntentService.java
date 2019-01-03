b6bcd887a09e2ae9c692576c12645c147156f251
Olivier Favre <olivier@wonderpush.com>
2019-01-03 18:15:11 +0100

    Cancel job on destroy in JobIntentService to avoid SecurityException

--- sdk/src/main/java/android/support/v4/app/WonderPushJobIntentService.java
+++ sdk/src/main/java/android/support/v4/app/WonderPushJobIntentService.java
@@ -3,9 +3,22 @@ package android.support.v4.app;
 import android.util.Log;
 
 // See: https://issuetracker.google.com/issues/63622293
+// See: https://github.com/optimizely/android-sdk/pull/193
 public abstract class WonderPushJobIntentService extends JobIntentService {
 
     private static final String TAG = "WPJobIntentService";
+
+    @Override
+    public void onDestroy() {
+        try {
+            if (mCurProcessor != null) {
+                mCurProcessor.cancel(true);
+            }
+        } catch (Exception ex) {
+            Log.e(TAG, "Unexpected error while in canceling current processor in onDestroy", ex);
+        }
+        super.onDestroy();
+    }
 
     @Override
     GenericWorkItem dequeueWork() {
