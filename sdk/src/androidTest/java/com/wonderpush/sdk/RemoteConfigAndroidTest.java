8c800c6341d868f957e833f8a8ebb5ce147035cc
Stéphane JAIS <stephane@wonderpush.com>
2020-05-18 15:27:05 +0200

    introducing instrumented tests

---
+++ sdk/src/androidTest/java/com/wonderpush/sdk/RemoteConfigAndroidTest.java
@@ -0,0 +1,24 @@
+package com.wonderpush.sdk;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class RemoteConfigAndroidTest {
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.wonderpush.sdk.test", appContext.getPackageName());
+    }
+
+}
