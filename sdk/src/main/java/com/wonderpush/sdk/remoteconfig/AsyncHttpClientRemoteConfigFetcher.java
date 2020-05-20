9cc5a8580f5d05ef8d5ca5065a69d55fe64ba407
Stéphane JAIS <stephane@wonderpush.com>
2020-05-20 10:36:43 +0200

    allow fetcher to run on a thread specified by caller.

--- sdk/src/main/java/com/wonderpush/sdk/remoteconfig/AsyncHttpClientRemoteConfigFetcher.java
+++ sdk/src/main/java/com/wonderpush/sdk/remoteconfig/AsyncHttpClientRemoteConfigFetcher.java
@@ -16,50 +16,59 @@ public class AsyncHttpClientRemoteConfigFetcher implements RemoteConfigFetcher {
     AsyncHttpClient client = new AsyncHttpClient(); // to allow any HTTPS certificate use: new AsyncHttpClient(true, 80, 443);
     @Nonnull
     String clientId;
-    public AsyncHttpClientRemoteConfigFetcher(String clientId) {
+    @Nonnull
+    private SafeDeferProvider safeDeferProvider;
+    public AsyncHttpClientRemoteConfigFetcher(String clientId, SafeDeferProvider safeDeferProvider) {
         this.clientId = clientId;
+        this.safeDeferProvider = safeDeferProvider;
+    }
+
+    public interface SafeDeferProvider {
+        void safeDefer(Runnable r, long defer);
     }
 
     @Override
     public void fetchRemoteConfig(@Nullable String version, @Nonnull RemoteConfigHandler handler) {
         String url = String.format("%s%s",Constants.REMOTE_CONFIG_BASE_URL, clientId);
-        client.get(url, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                if (statusCode != 200) {
-                    handler.handle(null, new Exception("Invalid status code from remote config server:" + statusCode));
-                    return;
+        this.safeDeferProvider.safeDefer(() -> {
+            client.get(url, new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    if (statusCode != 200) {
+                        handler.handle(null, new Exception("Invalid status code from remote config server:" + statusCode));
+                        return;
+                    }
+
+                    String version = response.optString("_configVersion");
+                    long maxAge = response.optLong("_configMaxAge", Constants.REMOTE_CONFIG_DEFAULT_MAXIMUM_CONFIG_AGE);
+                    if (version != null) {
+                        RemoteConfig config = RemoteConfig.with(response, version, DateHelper.now(), maxAge);
+                        handler.handle(config, null);
+                        return;
+                    }
+                    handler.handle(null, new Exception("Invalid remote config format"));
                 }
 
-                String version = response.optString("_configVersion");
-                long maxAge = response.optLong("_configMaxAge", Constants.REMOTE_CONFIG_DEFAULT_MAXIMUM_CONFIG_AGE);
-                if (version != null) {
-                    RemoteConfig config = RemoteConfig.with(response, version, DateHelper.now(), maxAge);
-                    handler.handle(config, null);
-                    return;
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                    handler.handle(null, new Exception("Invalid response from remote config server"));
                 }
-                handler.handle(null, new Exception("Invalid remote config format"));
-            }
 
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                handler.handle(null, new Exception("Invalid response from remote config server"));
-            }
+                @Override
+                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                    handler.handle(null, throwable);
+                }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                handler.handle(null, throwable);
-            }
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                    handler.handle(null, throwable);
+                }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
-                handler.handle(null, throwable);
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                handler.handle(null, throwable);
-            }
-        });
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                    handler.handle(null, throwable);
+                }
+            });
+        }, 0);
     }
 }
