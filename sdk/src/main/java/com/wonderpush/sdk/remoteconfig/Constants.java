bcdcfc970d6558a2531898964e09744ff209fea4
Stéphane JAIS <stephane@wonderpush.com>
2020-09-22 15:25:48 +0200

    no minimum config age

--- sdk/src/main/java/com/wonderpush/sdk/remoteconfig/Constants.java
+++ sdk/src/main/java/com/wonderpush/sdk/remoteconfig/Constants.java
@@ -1,7 +1,7 @@
 package com.wonderpush.sdk.remoteconfig;
 
 public class Constants {
-    public static final long REMOTE_CONFIG_DEFAULT_MINIMUM_CONFIG_AGE = 1800000;
+    public static final long REMOTE_CONFIG_DEFAULT_MINIMUM_CONFIG_AGE = 0;
     public static final long REMOTE_CONFIG_DEFAULT_MAXIMUM_CONFIG_AGE = 86400000 * 10;
     public static final String INTENT_REMOTE_CONFIG_UPDATED = "INTENT_REMOTE_CONFIG_UPDATED";
     public static final String EXTRA_REMOTE_CONFIG = "EXTRA_REMOTE_CONFIG";
