a06683a62d9b4f9f0fe10fdcd44902f11180e044
Olivier Favre <olivier@wonderpush.com>
2021-11-30 11:46:12 +0100

    Fix NPE when no targetUrl was given in the push notification

--- sdk/src/main/java/com/wonderpush/sdk/WonderPushNotificationTrackingReceiver.java
+++ sdk/src/main/java/com/wonderpush/sdk/WonderPushNotificationTrackingReceiver.java
@@ -40,31 +40,34 @@ public class WonderPushNotificationTrackingReceiver extends Activity {
             NotificationManager.handleOpenedNotificationFromTrackingActivity(this, intent, notif);
             ActivityLifecycleMonitor.onNextResume(activity -> WonderPush.showPotentialNotification(activity, intent));
 
-            Uri parsedTargetUrl = Uri.parse(notif.getTargetUrl());
-            if (WonderPush.INTENT_NOTIFICATION_SCHEME.equals(parsedTargetUrl.getScheme())
-                    && WonderPush.INTENT_NOTIFICATION_WILL_OPEN_AUTHORITY.equals(parsedTargetUrl.getAuthority())
-                    && parsedTargetUrl.getPathSegments().size() == 1
-                    && WonderPush.INTENT_NOTIFICATION_WILL_OPEN_PATH_BROADCAST.equals(parsedTargetUrl.getLastPathSegment())
-            ) {
-                // wonderpush://notificationOpen/broadcast
-                // Broadcast locally that a notification is to be opened, and don't do anything else
-                Intent notificationWillOpenIntent = new Intent();
-                notificationWillOpenIntent.setPackage(getApplicationContext().getPackageName());
-                notificationWillOpenIntent.setAction(WonderPush.INTENT_NOTIFICATION_WILL_OPEN);
-                //notificationWillOpenIntent.setData(Uri.parse(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_SCHEME + "://" + WonderPush.INTENT_NOTIFICATION_WILL_OPEN_AUTHORITY + "/" + WonderPush.INTENT_NOTIFICATION_WILL_OPEN_PATH_BROADCAST));
-                notificationWillOpenIntent.putExtra(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_EXTRA_RECEIVED_PUSH_NOTIFICATION,
-                        (Parcelable) intent.getParcelableExtra("receivedPushNotificationIntent"));
-                notificationWillOpenIntent.putExtra(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_EXTRA_NOTIFICATION_MODEL,
-                        // this extra must be removed if handled outside the app,
-                        // or we'll get ClassNotFoundException: E/Parcel: Class not found when unmarshalling: com.wonderpush.sdk.Notification*Model
-                        notif);
-                notificationWillOpenIntent.putExtra(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_EXTRA_NOTIFICATION_TYPE,
-                        notif.getType().toString());
-                notificationWillOpenIntent.putExtra(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_EXTRA_FROM_USER_INTERACTION,
-                        intent.getBooleanExtra("fromUserInteraction", true));
-                notificationWillOpenIntent.putExtra(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_EXTRA_AUTOMATIC_OPEN,
-                        true);
-                LocalBroadcastManager.getInstance(this).sendBroadcast(notificationWillOpenIntent);
+            String targetUrl = notif.getTargetUrl();
+            if (targetUrl != null) {
+                Uri parsedTargetUrl = Uri.parse(targetUrl);
+                if (WonderPush.INTENT_NOTIFICATION_SCHEME.equals(parsedTargetUrl.getScheme())
+                        && WonderPush.INTENT_NOTIFICATION_WILL_OPEN_AUTHORITY.equals(parsedTargetUrl.getAuthority())
+                        && parsedTargetUrl.getPathSegments().size() == 1
+                        && WonderPush.INTENT_NOTIFICATION_WILL_OPEN_PATH_BROADCAST.equals(parsedTargetUrl.getLastPathSegment())
+                ) {
+                    // wonderpush://notificationOpen/broadcast
+                    // Broadcast locally that a notification is to be opened, and don't do anything else
+                    Intent notificationWillOpenIntent = new Intent();
+                    notificationWillOpenIntent.setPackage(getApplicationContext().getPackageName());
+                    notificationWillOpenIntent.setAction(WonderPush.INTENT_NOTIFICATION_WILL_OPEN);
+                    //notificationWillOpenIntent.setData(Uri.parse(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_SCHEME + "://" + WonderPush.INTENT_NOTIFICATION_WILL_OPEN_AUTHORITY + "/" + WonderPush.INTENT_NOTIFICATION_WILL_OPEN_PATH_BROADCAST));
+                    notificationWillOpenIntent.putExtra(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_EXTRA_RECEIVED_PUSH_NOTIFICATION,
+                            (Parcelable) intent.getParcelableExtra("receivedPushNotificationIntent"));
+                    notificationWillOpenIntent.putExtra(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_EXTRA_NOTIFICATION_MODEL,
+                            // this extra must be removed if handled outside the app,
+                            // or we'll get ClassNotFoundException: E/Parcel: Class not found when unmarshalling: com.wonderpush.sdk.Notification*Model
+                            notif);
+                    notificationWillOpenIntent.putExtra(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_EXTRA_NOTIFICATION_TYPE,
+                            notif.getType().toString());
+                    notificationWillOpenIntent.putExtra(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_EXTRA_FROM_USER_INTERACTION,
+                            intent.getBooleanExtra("fromUserInteraction", true));
+                    notificationWillOpenIntent.putExtra(WonderPush.INTENT_NOTIFICATION_WILL_OPEN_EXTRA_AUTOMATIC_OPEN,
+                            true);
+                    LocalBroadcastManager.getInstance(this).sendBroadcast(notificationWillOpenIntent);
+                }
             }
         }
     }
