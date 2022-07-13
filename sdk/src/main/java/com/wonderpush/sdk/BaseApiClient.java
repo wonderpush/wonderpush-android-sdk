wonderpush/wonderpush-android-sdk

commit 01c231f4bad3beb8e661d85d81572291ceacc31e

Making ApiClient a singleton and extract BaseApiClient superclass

sdk/src/main/java/com/wonderpush/sdk/BaseApiClient.java
@@ -1,0 +1,376 @@
+package com.wonderpush.sdk;
+
+import android.net.TrafficStats;
+import android.os.Process;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.EventListener;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+
+public abstract class BaseApiClient implements WonderPushRequestVault.RequestExecutor {
+    private static final String TAG = "WonderPush." + BaseApiClient.class.getSimpleName();
+
+    private static final int RETRY_INTERVAL_BAD_AUTH = 1 * 1000; // in milliseconds
+    protected static final int ERROR_INVALID_CREDENTIALS = 11000;
+    protected static final int ERROR_INVALID_ACCESS_TOKEN = 11003;
+
+    private final WonderPushRequestVault requestVault = new WonderPushRequestVault(WonderPushJobQueue.getDefaultQueue(), this);
+    private final OkHttpClient client = new OkHttpClient.Builder().eventListener(new EventListener() {
+        @Override
+        public void connectStart(Call call, InetSocketAddress inetSocketAddress, Proxy proxy) {
+            TrafficStats.setThreadStatsTag(Process.myTid());
+        }
+    }).build();
+    private boolean disabled = false;
+
+    protected abstract void decorate(Request request);
+
+    /**
+     * A request that is guaranteed to be executed when a network connection
+     * is present, surviving application reboot. The responseHandler will be
+     * called only if the network is present when the request is first run.
+     *
+     * @param request
+     */
+    protected void requestEventually(Request request) {
+        requestVault.put(request, 0);
+    }
+
+    /**
+     * A request
+     *
+     * @param userId
+     *            The userId to perform this request for
+     * @param method
+     *            The HTTP method to use
+     * @param resource
+     *            The resource path, starting with /
+     * @param params
+     *            Request parameters
+     * @param responseHandler
+     *            Response handler
+     */
+    protected void requestForUser(String userId, HttpMethod method, String resource, Request.Params params, ResponseHandler responseHandler) {
+        request(new Request(userId, method, resource, params, responseHandler));
+    }
+
+    /**
+     * A GET request
+     *
+     * @param resource
+     *            The resource path, starting with /
+     * @param params
+     *            Request parameters
+     * @param responseHandler
+     *            Response handler
+     */
+    protected void get(String resource, Request.Params params, ResponseHandler responseHandler) {
+        request(new Request(WonderPushConfiguration.getUserId(), HttpMethod.GET, resource, params, responseHandler));
+    }
+
+    /**
+     * A POST request
+     *
+     * @param resource
+     *            The resource path, starting with /
+     * @param params
+     *            Request parameters
+     * @param responseHandler
+     *            Response handler
+     */
+    protected void post(String resource, Request.Params params, ResponseHandler responseHandler) {
+        request(new Request(WonderPushConfiguration.getUserId(), HttpMethod.POST, resource, params, responseHandler));
+    }
+
+    /**
+     * A POST request that is guaranteed to be executed when a network connection
+     * is present, surviving application reboot. The responseHandler will be
+     * called only if the network is present when the request is first run.
+     *
+     * @param resource
+     *            The resource path, starting with /
+     * @param params
+     *            Request parameters
+     */
+    protected void postEventually(String resource, Request.Params params) {
+        final Request request = new Request(WonderPushConfiguration.getUserId(), HttpMethod.POST, resource, params, null);
+        requestEventually(request);
+    }
+
+    /**
+     * A PUT request
+     *
+     * @param resource
+     *            The resource path, starting with /
+     * @param params
+     *            Request parameters
+     * @param responseHandler
+     *            Response handler
+     */
+    protected void put(String resource, Request.Params params, ResponseHandler responseHandler) {
+        request(new Request(WonderPushConfiguration.getUserId(), HttpMethod.PUT, resource, params, responseHandler));
+    }
+
+    /**
+     * A DELETE request
+     *
+     * @param resource
+     *            The resource path, starting with /
+     * @param responseHandler
+     *            Response handler
+     */
+    protected void delete(String resource, ResponseHandler responseHandler) {
+        request(new Request(WonderPushConfiguration.getUserId(), HttpMethod.DELETE, resource, null, responseHandler));
+    }
+
+
+    /**
+     * Runs the specified request.
+     */
+    public void execute(final Request request) {
+        if (null == request) {
+            return;
+        }
+
+        if (!WonderPush.isInitialized()) {
+            WonderPush.safeDefer(() -> request(request), 100);
+            return;
+        }
+
+        // Add the access token to the params
+        Request.Params params = request.getParams();
+        if (null == params) {
+            params = new Request.Params();
+            request.setParams(params);
+        }
+
+        // Wrap the request handler with our own
+        ResponseHandler wrapperHandler = new ResponseHandler() {
+            @Override
+            public void onSuccess(int status, Response response) {
+                WonderPush.logDebug(TAG, "Request successful: (" + status + ") " + response + " (for " + request.toHumanReadableString() + ")");
+                if (request.getHandler() != null) {
+                    request.getHandler().onSuccess(status, response);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable e, Response errorResponse) {
+                WonderPush.logError(TAG, "Request failed: " + errorResponse + " (for " + request.toHumanReadableString() + ")", e);
+                if (errorResponse != null && ERROR_INVALID_ACCESS_TOKEN == errorResponse.getErrorCode()) {
+                    // null out the access token
+                    WonderPushConfiguration.invalidateCredentials();
+
+                    // retry later now
+                    WonderPush.safeDefer(() -> request(request), RETRY_INTERVAL_BAD_AUTH);
+                } else {
+                    if (request.getHandler() != null) {
+                        request.getHandler().onFailure(e, errorResponse);
+                    }
+                }
+            }
+
+            @Override
+            public void onSuccess(Response response) {
+                WonderPush.logDebug(TAG, "Request successful: " + response + " (for " + request.toHumanReadableString() + ")");
+                if (request.getHandler() != null) {
+                    request.getHandler().onSuccess(response);
+                }
+            }
+        };
+        Request wrapperRequest = (Request) request.clone();
+        wrapperRequest.setHandler(wrapperHandler);
+
+        // Perform request
+        request(wrapperRequest);
+    }
+
+    /**
+     * Thin wrapper to the network library.
+     */
+    protected void request(final Request request) {
+        if (null == request) {
+            WonderPush.logError(TAG, "Request with null request.");
+            return;
+        }
+
+        WonderPush.safeDefer(() -> {
+            // Decorate parameters
+            WonderPushRequestParamsDecorator.decorate(request.getResource(), request.getParams());
+            decorate(request);
+
+            // Generate signature
+            Request.BasicNameValuePair authorizationHeader = request.getAuthorizationHeader();
+
+            WonderPush.logDebug(TAG, "Requesting: " + request.toHumanReadableString());
+            // TODO: support other contentTypes such as "application/json"
+            String contentType = "application/x-www-form-urlencoded";
+
+            // Handler
+            final @NonNull ResponseHandler handler = new ResponseHandler() {
+                @Override
+                public void onFailure(Throwable e, Response errorResponse) {
+                    WonderPush.safeDefer(() -> {
+                        if (request.getHandler() != null) {
+                            request.getHandler().onFailure(e, errorResponse);
+                        }
+                    }, 0);
+                }
+
+                @Override
+                public void onSuccess(Response response) {
+                    WonderPush.safeDefer(() -> {
+                        if (request.getHandler() != null) {
+                            request.getHandler().onSuccess(response);
+                        }
+                    }, 0);
+                }
+            };
+
+            final long sendDate = SystemClock.elapsedRealtime();
+            Callback jsonHandler = new SafeOkHttpCallback() {
+
+                @Override
+                public void onFailureSafe(Call call, IOException e) {
+                    handler.onFailure(e, new Response((JSONObject)null));
+                }
+
+                @Override
+                public void onResponseSafe(Call call, okhttp3.Response response) throws IOException {
+                    // Try parse JSON
+                    String responseString = response.body().string();
+                    JSONObject responseJson;
+                    try {
+                        responseJson = new JSONObject(responseString);
+                    } catch (JSONException e) {
+                        if (WonderPush.getLogging()) {
+                            Log.e(TAG, "Unexpected string error answer: " + response.code() + " headers: " + response.headers() + " response: (" + responseString.length() + ") \"" + responseString + "\" (for " + request.toHumanReadableString() + ")", e);
+                        } else {
+                            Log.e(TAG, "Unexpected string error answer: " + response.code() + " headers: " + response.headers() + " response: (" + responseString.length() + ") \"" + responseString + "\"", e);
+                        }
+                        handler.onFailure(e, new Response(responseString));
+                        return;
+                    }
+
+                    syncTime(responseJson);
+                    declareConfigVersion(responseJson);
+                    if (!response.isSuccessful()) {
+                        if (WonderPush.getLogging()) {
+                            Log.e(TAG, "Error answer: " + response.code() + " headers: " + response.headers() + " response: " + responseString + " (for " + request.toHumanReadableString() + ")");
+                        } else {
+                            Log.e(TAG, "Error answer: " + response.code() + " headers: " + response.headers() + " response: " + responseString);
+                        }
+                        handler.onFailure(null, new Response(responseJson));
+                    } else {
+                        handler.onSuccess(response.code(), new Response(responseJson));
+                    }
+
+                }
+
+                private void declareConfigVersion(JSONObject data) {
+                    if (data == null || !data.has("_configVersion") || data.isNull("_configVersion")) return;
+                    String version = data.optString("_configVersion", Long.toString(data.optLong("_configVersion", 0)));
+                    if (version != null && WonderPush.getRemoteConfigManager() != null) {
+                        WonderPush.getRemoteConfigManager().declareVersion(version);
+                    }
+                }
+
+                private void syncTime(JSONObject data) {
+                    long recvDate = SystemClock.elapsedRealtime();
+                    if (data == null || !data.has("_serverTime")) {
+                        return;
+                    }
+                    TimeSync.syncTimeWithServer(sendDate, recvDate, data.optLong("_serverTime"), data.optLong("_serverTook"));
+                }
+            };
+
+            HttpUrl.Builder httpUrlBuilder = HttpUrl.parse(WonderPushUriHelper.getAbsoluteUrl(request.getResource())).newBuilder();
+            okhttp3.Request.Builder requestBuilder = new okhttp3.Request.Builder()
+                    .addHeader("Content-Type", contentType);
+            if (authorizationHeader != null) {
+                requestBuilder.addHeader(authorizationHeader.getName(),authorizationHeader.getValue());
+            }
+
+            // NO UNNECESSARY WORK HERE, because of timed request
+            switch (request.getMethod()) {
+                case GET: {
+                    if (request.getParams() != null) {
+                        for (Request.BasicNameValuePair pair : request.getParams().getParamsList()) {
+                            httpUrlBuilder.addQueryParameter(pair.getName(), pair.getValue());
+                        }
+                    }
+                    requestBuilder.url(httpUrlBuilder.build());
+                    requestBuilder.get();
+                }
+                break;
+                case PUT: {
+                    requestBuilder.url(httpUrlBuilder.build());
+                    if (request.getParams() != null) {
+                        requestBuilder.put(request.getParams().getFormBody());
+                    }
+                }
+                break;
+                case POST:
+                    requestBuilder.url(httpUrlBuilder.build());
+                    if (request.getParams() != null) {
+                        requestBuilder.post(request.getParams().getFormBody());
+                    }
+                    break;
+                case PATCH:
+                    requestBuilder.url(httpUrlBuilder.build());
+                    if (request.getParams() != null) {
+                        requestBuilder.patch(request.getParams().getFormBody());
+                    }
+                    break;
+                case DELETE: {
+                    if (request.getParams() != null) {
+                        for (Request.BasicNameValuePair pair : request.getParams().getParamsList()) {
+                            httpUrlBuilder.addQueryParameter(pair.getName(), pair.getValue());
+                        }
+                    }
+                    requestBuilder.url(httpUrlBuilder.build());
+                    requestBuilder.delete();
+                }
+                break;
+                default:
+                    requestBuilder = null;
+                    if (handler != null) {
+                        handler.onFailure(new UnsupportedOperationException("Unhandled method " + request.getMethod()), null);
+                    }
+                    break;
+            }
+
+            if (isDisabled()) {
+                handler.onFailure(new Request.ClientDisabledException(), new Response("Client disabled"));
+                return;
+            }
+            if (requestBuilder != null) {
+                client.newCall(requestBuilder.build()).enqueue(jsonHandler);
+            }
+
+        }, 0);
+    }
+
+
+    void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+
+    boolean isDisabled() {
+        return disabled;
+    }
+
+}

sdk/src/main/java/com/wonderpush/sdk/DataManager.java
@@ -64,7 +64,7 @@ class DataManager {
                 step1AccessToken.set(new Runnable() {
                     @Override
                     public void run() {
-                        ApiClient.requestForUser(currentUserId.get(), ApiClient.HttpMethod.GET, "/authentication/accessToken", null, new ResponseHandler() {
+                        ApiClient.getInstance().requestForUser(currentUserId.get(), HttpMethod.GET, "/authentication/accessToken", null, new ResponseHandler() {
                             @Override
                             public void onFailure(Throwable e, Response errorResponse) {
                                 sb.append("{\"accessToken\":");
@@ -91,7 +91,7 @@ class DataManager {
                             WonderPush.safeDefer(step3Installation.get(), 0);
                             return;
                         }
-                        ApiClient.requestForUser(currentUserId.get(), ApiClient.HttpMethod.GET, "/user", null, new ResponseHandler() {
+                        ApiClient.getInstance().requestForUser(currentUserId.get(), HttpMethod.GET, "/user", null, new ResponseHandler() {
                             @Override
                             public void onFailure(Throwable e, Response errorResponse) {
                                 sb.append("{\"user\":");
@@ -114,7 +114,7 @@ class DataManager {
                 step3Installation.set(new Runnable() {
                     @Override
                     public void run() {
-                        ApiClient.requestForUser(currentUserId.get(), ApiClient.HttpMethod.GET, "/installation", null, new ResponseHandler() {
+                        ApiClient.getInstance().requestForUser(currentUserId.get(), HttpMethod.GET, "/installation", null, new ResponseHandler() {
                             @Override
                             public void onFailure(Throwable e, Response errorResponse) {
                                 sb.append("{\"installation\":");
@@ -138,7 +138,7 @@ class DataManager {
                 step4EventPage.set(new Runnable() {
                     @Override
                     public void run() {
-                        ApiClient.requestForUser(currentUserId.get(), ApiClient.HttpMethod.GET, "/events", step4RequestParams.get(), new ResponseHandler() {
+                        ApiClient.getInstance().requestForUser(currentUserId.get(), HttpMethod.GET, "/events", step4RequestParams.get(), new ResponseHandler() {
                             @Override
                             public void onFailure(Throwable e, Response errorResponse) {
                                 sb.append("{\"eventsPage\":");
@@ -241,7 +241,7 @@ class DataManager {
     }
 
     private static void clearEventsHistory(String userId) {
-        ApiClient.requestForUser(userId, ApiClient.HttpMethod.DELETE, "/events", null, null);
+        ApiClient.getInstance().requestForUser(userId, HttpMethod.DELETE, "/events", null, null);
     }
 
     static void clearPreferences() {
@@ -263,12 +263,12 @@ class DataManager {
         }
 
         if (userId != null) {
-            ApiClient.requestForUser(userId, ApiClient.HttpMethod.PUT, "/user", new Request.Params("body", "{\"custom\":null}"), null);
+            ApiClient.getInstance().requestForUser(userId, HttpMethod.PUT, "/user", new Request.Params("body", "{\"custom\":null}"), null);
         }
     }
 
     private static void clearInstallation(String userId) {
-        ApiClient.requestForUser(userId, ApiClient.HttpMethod.DELETE, "/installation", null, null);
+        ApiClient.getInstance().requestForUser(userId, HttpMethod.DELETE, "/installation", null, null);
         WonderPushConfiguration.clearForUserId(userId);
         try {
             JSONSyncInstallation.forUser(userId).receiveState(null, true);

sdk/src/main/java/com/wonderpush/sdk/HttpMethod.java
@@ -1,0 +1,10 @@
+package com.wonderpush.sdk;
+
+enum HttpMethod {
+    GET,
+    PUT,
+    POST,
+    DELETE,
+    PATCH,
+    // APPEND ONLY!
+}

sdk/src/main/java/com/wonderpush/sdk/JSONSyncInstallation.java
@@ -270,7 +270,7 @@ public class JSONSyncInstallation {
         WonderPush.logDebug("Sending installation custom diff " + diff + " for user " + userId);
         Request.Params parameters = new Request.Params();
         parameters.put("body", diff.toString());
-        ApiClient.requestForUser(userId, ApiClient.HttpMethod.PATCH, "/installation", parameters, new ResponseHandler() {
+        ApiClient.getInstance().requestForUser(userId, HttpMethod.PATCH, "/installation", parameters, new ResponseHandler() {
             @Override
             public void onFailure(Throwable ex, Response errorResponse) {
 

sdk/src/main/java/com/wonderpush/sdk/MeasurementsApiClient.java
@@ -29,7 +29,7 @@ public class MeasurementsApiClient {
             return;
         }
 
-        ApiClient.HttpMethod method = request.getMethod();
+        HttpMethod method = request.getMethod();
         String resource = request.getResource();
         String url = String.format("%s%s", WonderPush.MEASUREMENTS_API_URL, resource);
         String contentType = "application/x-www-form-urlencoded";
@@ -107,7 +107,7 @@ public class MeasurementsApiClient {
         MeasurementsApiClient.disabled = disabled;
     }
 
-    private static String humanReadableRequest(ApiClient.HttpMethod method, String resource, Request.Params params) {
+    private static String humanReadableRequest(HttpMethod method, String resource, Request.Params params) {
         return method + " " + resource + "?" + params.toHumanReadableString();
     }
 

sdk/src/main/java/com/wonderpush/sdk/Request.java
@@ -26,12 +26,12 @@ class Request {
     private static final String TAG = "WonderPush." + Request.class.getSimpleName();
 
     String mUserId;
-    ApiClient.HttpMethod mMethod;
+    HttpMethod mMethod;
     Params mParams;
     ResponseHandler mHandler;
     String mResource;
 
-    public Request(String userId, ApiClient.HttpMethod method, String resource, Params params, ResponseHandler handler) {
+    public Request(String userId, HttpMethod method, String resource, Params params, ResponseHandler handler) {
         mUserId = userId;
         mMethod = method;
         mParams = params;
@@ -42,9 +42,9 @@ class Request {
     public Request(JSONObject data) throws JSONException {
         mUserId = data.has("userId") ? JSONUtil.getString(data, "userId") : WonderPushConfiguration.getUserId();
         try {
-            mMethod = ApiClient.HttpMethod.valueOf(JSONUtil.getString(data, "method"));
+            mMethod = HttpMethod.valueOf(JSONUtil.getString(data, "method"));
         } catch (IllegalArgumentException ex) {
-            mMethod = ApiClient.HttpMethod.values()[data.getInt("method")];
+            mMethod = HttpMethod.values()[data.getInt("method")];
         }
         mResource = data.getString("resource");
         JSONObject paramsJson = data.getJSONObject("params");
@@ -81,7 +81,7 @@ class Request {
         return mUserId;
     }
 
-    public ApiClient.HttpMethod getMethod() {
+    public HttpMethod getMethod() {
         return mMethod;
     }
 
@@ -97,7 +97,7 @@ class Request {
         return mResource;
     }
 
-    public void setMethod(ApiClient.HttpMethod mMethod) {
+    public void setMethod(HttpMethod mMethod) {
         this.mMethod = mMethod;
     }
 
@@ -127,7 +127,7 @@ class Request {
         return getAuthorizationHeader(mMethod, Uri.parse(String.format("%s%s", WonderPush.getBaseURL(), mResource)), mParams);
     }
 
-    protected static BasicNameValuePair getAuthorizationHeader(ApiClient.HttpMethod method, Uri uri, Params params) {
+    protected static BasicNameValuePair getAuthorizationHeader(HttpMethod method, Uri uri, Params params) {
         try {
             StringBuilder sb = new StringBuilder();
 

commit c6416b6069bb16dc76d678eeb9f92646a1922d1c

Fix bug introduced in 01c231f4bad3beb8e661d85d81572291ceacc31e: re-enter with "execute", not "request"

sdk/src/main/java/com/wonderpush/sdk/BaseApiClient.java
@@ -64,7 +64,7 @@ public abstract class BaseApiClient implements WonderPushRequestVault.RequestExe
      *            Response handler
      */
     protected void requestForUser(String userId, HttpMethod method, String resource, Request.Params params, ResponseHandler responseHandler) {
-        request(new Request(userId, method, resource, params, responseHandler));
+        execute(new Request(userId, method, resource, params, responseHandler));
     }
 
     /**
@@ -78,7 +78,7 @@ public abstract class BaseApiClient implements WonderPushRequestVault.RequestExe
      *            Response handler
      */
     protected void get(String resource, Request.Params params, ResponseHandler responseHandler) {
-        request(new Request(WonderPushConfiguration.getUserId(), HttpMethod.GET, resource, params, responseHandler));
+        execute(new Request(WonderPushConfiguration.getUserId(), HttpMethod.GET, resource, params, responseHandler));
     }
 
     /**
@@ -92,7 +92,7 @@ public abstract class BaseApiClient implements WonderPushRequestVault.RequestExe
      *            Response handler
      */
     protected void post(String resource, Request.Params params, ResponseHandler responseHandler) {
-        request(new Request(WonderPushConfiguration.getUserId(), HttpMethod.POST, resource, params, responseHandler));
+        execute(new Request(WonderPushConfiguration.getUserId(), HttpMethod.POST, resource, params, responseHandler));
     }
 
     /**
@@ -121,7 +121,7 @@ public abstract class BaseApiClient implements WonderPushRequestVault.RequestExe
      *            Response handler
      */
     protected void put(String resource, Request.Params params, ResponseHandler responseHandler) {
-        request(new Request(WonderPushConfiguration.getUserId(), HttpMethod.PUT, resource, params, responseHandler));
+        execute(new Request(WonderPushConfiguration.getUserId(), HttpMethod.PUT, resource, params, responseHandler));
     }
 
     /**
@@ -133,9 +133,8 @@ public abstract class BaseApiClient implements WonderPushRequestVault.RequestExe
      *            Response handler
      */
     protected void delete(String resource, ResponseHandler responseHandler) {
-        request(new Request(WonderPushConfiguration.getUserId(), HttpMethod.DELETE, resource, null, responseHandler));
+        execute(new Request(WonderPushConfiguration.getUserId(), HttpMethod.DELETE, resource, null, responseHandler));
     }
-
 
     /**
      * Runs the specified request.
@@ -175,7 +174,7 @@ public abstract class BaseApiClient implements WonderPushRequestVault.RequestExe
                     WonderPushConfiguration.invalidateCredentials();
 
                     // retry later now
-                    WonderPush.safeDefer(() -> request(request), RETRY_INTERVAL_BAD_AUTH);
+                    WonderPush.safeDefer(() -> execute(request), RETRY_INTERVAL_BAD_AUTH);
                 } else {
                     if (request.getHandler() != null) {
                         request.getHandler().onFailure(e, errorResponse);