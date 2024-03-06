9cdb023465e747ec73c8a4339e9e38503c9e7f07
Olivier Favre <olivier@wonderpush.com>
2024-03-06 10:50:39 +0100

    Revert "Documentation site for v4.3.5"
    This reverts commit f9ba50fbdecfef296c7a693f634a2fc65f08531a.

--- 4.3.5/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.wonderpush.sdk"},{"l":"com.wonderpush.sdk.inappmessaging"},{"l":"com.wonderpush.sdk.inappmessaging.model"}];updateSearchResults();
