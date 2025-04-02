b21b1a2fd81df2f72388c0de89fded43a0c72d8e
Olivier Favre <olivier@wonderpush.com>
2025-04-02 17:55:53 +0200

    Remove unused import

--- sdk/src/main/java/com/wonderpush/sdk/InstallationManager.java
+++ sdk/src/main/java/com/wonderpush/sdk/InstallationManager.java
@@ -1,6 +1,5 @@
 package com.wonderpush.sdk;
 
-import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
